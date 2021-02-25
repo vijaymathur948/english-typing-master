83993d61d06d64ea3abcf23732504dd3264cbdd9
vijay-mathur <vijaymathur948@gmail.com>
2021-02-25 19:43:29 +0530

    some changes

--- src/Components/TextBox.js
+++ src/Components/TextBox.js
@@ -5,34 +5,67 @@ class TextBox extends Component {
     super(props)
     this.state = {
       text: "",
+      data: "",
     }
     this.changeText = this.changeText.bind(this)
+  }
+  componentDidMount() {
+    this.toBottom()
   }
   changeText = e => {
     var textValue = e.target.value
     this.setState({ text: textValue })
   }
   onKeyPress = e => {
-    console.log(e.which)
-    if (e.which === 32) this.setState({ text: "" })
+    if (e.which === 32) {
+      this.setState({ data: this.state.data + this.state.text })
+      this.setState({ text: "" })
+      this.toBottom()
+    }
+  }
+  toBottom = () => {
+    var element = document.getElementById("data")
+    var height = element.scrollHeight
+    element.scrollTop = height
   }
   render() {
-    console.log(this.state.text)
     return (
-      <div className='center mt-5'>
-        <input
+      <>
+        <div
+          id='data'
           style={{
-            width: "30%",
+            fontSize: "30px",
+            margin: "20px",
             padding: "20px",
-            fontSize: "40px",
+            paddingBottom: "60px",
+            maxHeight: "400px",
+            overflow: "auto",
+            border: "1px solid gray",
           }}
-          value={this.state.text}
-          type='text'
-          placeholder='Start Typing'
-          onChange={this.changeText}
-          onKeyPress={this.onKeyPress}
-        />
-      </div>
+        >
+          {this.state.data}
+        </div>
+        <h1>
+          Total Characters: {this.state.data.length} Total Words:{" "}
+          {this.state.data.trim().split(" ").length}
+        </h1>
+        <div className='mx-auto' style={{ width: "35%", marginTop: "5%" }}>
+          <input
+            className='form-control'
+            style={{
+              width: "500px",
+              padding: "20px",
+              fontSize: "40px",
+            }}
+            value={this.state.text}
+            type='text'
+            placeholder='Start Typing...'
+            autoFocus={true}
+            onChange={this.changeText}
+            onKeyPress={this.onKeyPress}
+          />
+        </div>
+      </>
     )
   }
 }
